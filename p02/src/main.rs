dbfcbec3c0d11d167f8b1c50ebf7f55683176252
Ryan Collingham <ryan@tokencard.io>
2020-12-02 17:23:35 +0000

    solve part 2 of p02

--- p02/src/main.rs
+++ p02/src/main.rs
@@ -4,15 +4,25 @@ use std::fs;
 
 fn main() {
     let input = load_input();
-    let result = count_valid(&input);
-    println!("there are {} valid passwords", result);
+
+    let result_1 = count_valid(&input, validate_password_1);
+    println!(
+        "there are {} valid passwords using the first scheme",
+        result_1,
+    );
+
+    let result_2 = count_valid(&input, validate_password_2);
+    println!(
+        "there are {} valid passwords using the second scheme",
+        result_2,
+    );
 }
 
 struct PasswordInput {
     password: String,
     policy_letter: char,
-    min_letter_count: usize,
-    max_letter_count: usize,
+    policy_num_1: usize,
+    policy_num_2: usize,
 }
 
 fn load_input() -> Vec<PasswordInput> {
@@ -35,26 +45,39 @@ fn parse_line(line: &str, re: &Regex) -> PasswordInput {
     let cap = re.captures(line).unwrap();
 
     PasswordInput {
-        min_letter_count: cap[1].parse().unwrap(),
-        max_letter_count: cap[2].parse().unwrap(),
+        policy_num_1: cap[1].parse().unwrap(),
+        policy_num_2: cap[2].parse().unwrap(),
         policy_letter: cap[3].chars().next().unwrap(),
         password: cap[4].to_owned(),
     }
 }
 
-fn count_valid(input: &Vec<PasswordInput>) -> usize {
+fn count_valid(input: &Vec<PasswordInput>, validate_password: fn(&PasswordInput) -> bool) -> usize {
     input
         .iter()
         .filter(|&pass_input| validate_password(pass_input))
         .count()
 }
 
-fn validate_password(pass_input: &PasswordInput) -> bool {
+fn validate_password_1(pass_input: &PasswordInput) -> bool {
     let letter_count = pass_input
         .password
         .chars()
         .filter(|&letter| letter == pass_input.policy_letter)
         .count();
 
-    letter_count >= pass_input.min_letter_count && letter_count <= pass_input.max_letter_count
+    letter_count >= pass_input.policy_num_1 && letter_count <= pass_input.policy_num_2
+}
+
+fn validate_password_2(pass_input: &PasswordInput) -> bool {
+    let letter_1 = match pass_input.password.chars().nth(pass_input.policy_num_1 - 1) {
+        Some(val) => val,
+        None => return false,
+    };
+    let letter_2 = match pass_input.password.chars().nth(pass_input.policy_num_2 - 1) {
+        Some(val) => val,
+        None => return false,
+    };
+
+    (letter_1 == pass_input.policy_letter) != (letter_2 == pass_input.policy_letter)
 }

e867d2e256f9a0371d20f48858f462a1eb820f0f
Ryan Collingham <ryan@tokencard.io>
2020-12-03 13:10:55 +0000

    Solve part 2 of p03

--- p03/src/main.rs
+++ p03/src/main.rs
@@ -3,10 +3,24 @@ use std::fs;
 
 const TREE: char = '#';
 
+const PATHS: [Path; 5] = [
+    Path { right: 1, down: 1 },
+    Path { right: 3, down: 1 },
+    Path { right: 5, down: 1 },
+    Path { right: 7, down: 1 },
+    Path { right: 1, down: 2 },
+];
+
 fn main() {
     let input = load_input();
-    let num_trees = count_path_trees(&input);
-    println!("encountered {} trees", num_trees)
+    let num_trees = count_path_trees(&input, &Path { right: 3, down: 1 });
+    println!("encountered {} trees on default path", num_trees);
+
+    let path_multiple = get_path_multiples(&input);
+    println!(
+        "multiple of number of trees on all paths is {}",
+        path_multiple
+    );
 }
 
 fn load_input() -> Vec<Vec<char>> {
@@ -25,16 +39,29 @@ fn load_input() -> Vec<Vec<char>> {
         .collect()
 }
 
-fn count_path_trees(input: &Vec<Vec<char>>) -> i32 {
-    let mut count: i32 = 0;
-    let mut col: usize = 0;
+struct Path {
+    right: usize,
+    down: usize,
+}
 
-    for row in input {
-        if row[col] == TREE {
+fn count_path_trees(input: &Vec<Vec<char>>, path: &Path) -> u64 {
+    let mut count: u64 = 0;
+    let mut col_ix: usize = 0;
+
+    for row_ix in (0..input.len()).step_by(path.down) {
+        let row = &input[row_ix];
+        if row[col_ix] == TREE {
             count += 1;
         }
-        col = (col + 3) % row.len();
+        col_ix = (col_ix + path.right) % row.len();
     }
 
-    return count;
+    count
+}
+
+fn get_path_multiples(input: &Vec<Vec<char>>) -> u64 {
+    PATHS
+        .iter()
+        .map(|path| count_path_trees(input, path))
+        .product()
 }

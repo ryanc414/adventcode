2b2d5f872c64c81735633997563ad983ac2ef562
Ryan Collingham <ryan@tokencard.io>
2020-12-05 00:25:52 +0000

    Solve part 2 of p04

--- p04/src/main.rs
+++ p04/src/main.rs
@@ -1,21 +1,38 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 
-const REQUIRED_KEYS: [&str; 7] = [
-    "byr", // (Birth Year)
-    "iyr", // (Issue Year)
-    "eyr", // (Expiration Year)
-    "hgt", // (Height)
-    "hcl", // (Hair Color)
-    "ecl", // (Eye Color)
-    "pid", // (Passport ID)
-];
-
 fn main() {
     let input = load_input();
-    let count = count_valid(&input);
-    println!("counted {} valid passports", count);
+
+    let full_validation_rules_raw: &[(&str, fn(&str) -> bool)] = &[
+        ("byr", validate_birth_year),
+        ("iyr", validate_issue_year),
+        ("eyr", validate_expiration_year),
+        ("hgt", validate_height),
+        ("hcl", validate_hair_colour),
+        ("ecl", validate_eye_colour),
+        ("pid", validate_passport_id),
+    ];
+
+    let full_validation_rules: HashMap<&str, fn(&str) -> bool> =
+        full_validation_rules_raw.iter().cloned().collect();
+
+    let simple_validation_rules: HashMap<&str, fn(&str) -> bool> = full_validation_rules
+        .iter()
+        .map(|(&key, _)| -> (&str, fn(&str) -> bool) { (key, not_empty) })
+        .collect();
+
+    let count_1 = count_valid(&input, &simple_validation_rules);
+    println!("counted {} valid passports with simple scheme", count_1);
+
+    let count_2 = count_valid(&input, &full_validation_rules);
+    println!("counted {} valid passports with full scheme", count_2);
+}
+
+fn not_empty(input: &str) -> bool {
+    !input.is_empty()
 }
 
 fn load_input() -> Vec<HashMap<String, String>> {
@@ -46,16 +63,109 @@ fn parse_line(line: &str) -> HashMap<String, String> {
         .collect()
 }
 
-fn count_valid(input: &[HashMap<String, String>]) -> usize {
+fn count_valid(
+    input: &[HashMap<String, String>],
+    validation_rules: &HashMap<&str, fn(&str) -> bool>,
+) -> usize {
     input
         .iter()
-        .filter(|&passport| validate_passport(passport))
+        .filter(|&passport| validate_passport(passport, validation_rules))
         .count()
 }
 
-fn validate_passport(passport: &HashMap<String, String>) -> bool {
-    REQUIRED_KEYS.iter().all(|&key| match passport.get(key) {
-        Some(val) => !val.is_empty(),
-        None => false,
-    })
+fn validate_passport(
+    passport: &HashMap<String, String>,
+    rules: &HashMap<&str, fn(&str) -> bool>,
+) -> bool {
+    rules
+        .iter()
+        .all(|(&key, validate)| match passport.get(key) {
+            Some(val) => validate(val),
+            None => false,
+        })
+}
+
+fn validate_birth_year(input: &str) -> bool {
+    if input.len() != 4 {
+        return false;
+    }
+
+    let birth_year: u32 = match input.parse() {
+        Ok(year) => year,
+        Err(_) => return false,
+    };
+
+    birth_year >= 1920 && birth_year <= 2002
+}
+
+fn validate_issue_year(input: &str) -> bool {
+    if input.len() != 4 {
+        return false;
+    }
+
+    let issue_year: u32 = match input.parse() {
+        Ok(year) => year,
+        Err(_) => return false,
+    };
+
+    issue_year >= 2010 && issue_year <= 2020
+}
+
+fn validate_expiration_year(input: &str) -> bool {
+    if input.len() != 4 {
+        return false;
+    }
+
+    let expiration_year: u32 = match input.parse() {
+        Ok(year) => year,
+        Err(_) => return false,
+    };
+
+    expiration_year >= 2020 && expiration_year <= 2030
+}
+
+enum Unit {
+    CM,
+    Inches,
+}
+
+fn validate_height(input: &str) -> bool {
+    let re = Regex::new(r"^(\d+)(cm|in)$").unwrap();
+    let (raw_height, raw_unit) = match re.captures(input) {
+        Some(cap) => (cap[1].to_owned(), cap[2].to_owned()),
+        None => return false,
+    };
+
+    let height: u32 = match raw_height.parse() {
+        Ok(num) => num,
+        Err(_) => return false,
+    };
+
+    let unit = match raw_unit.as_str() {
+        "cm" => Unit::CM,
+        "in" => Unit::Inches,
+        _ => panic!("how did this happen"),
+    };
+
+    match unit {
+        Unit::CM => height >= 150 && height <= 193,
+        Unit::Inches => height >= 59 && height <= 76,
+    }
+}
+
+fn validate_hair_colour(input: &str) -> bool {
+    let re = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+    re.is_match(input)
+}
+
+fn validate_eye_colour(input: &str) -> bool {
+    match input {
+        "amb" | "blu" | "brn" | "gry" | "grn" | "hzl" | "oth" => true,
+        _ => false,
+    }
+}
+
+fn validate_passport_id(input: &str) -> bool {
+    let re = Regex::new(r"^\d{9}$").unwrap();
+    re.is_match(input)
 }

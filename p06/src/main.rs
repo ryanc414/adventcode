1c2cea52a730aa4e30a18538fb301cdc2895af11
Ryan Collingham <ryan@tokencard.io>
2020-12-06 18:21:15 +0000

    Implement AlphabetSet to avoid using HashSet

--- p06/src/main.rs
+++ p06/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::env;
 use std::fs;
 
@@ -30,15 +29,39 @@ fn load_input() -> Vec<Vec<Vec<char>>> {
         .collect()
 }
 
+// Instead of using a general-purpose HashSet, we can implement a set based on
+// a 26-element array since we know that all answers will be lowercase letters
+// 'a' to 'z'. This saves on unnecessary allocations and hashing.
+struct AlphabetSet([u8; 26]);
+
+impl AlphabetSet {
+    fn new() -> AlphabetSet {
+        AlphabetSet([0; 26])
+    }
+
+    fn insert(&mut self, element: char) {
+        if element < 'a' || element > 'z' {
+            panic!("cannot insert element {}", element);
+        }
+
+        let ix: usize = element as usize - 'a' as usize;
+        self.0[ix] = 1;
+    }
+
+    fn len(&self) -> usize {
+        self.0.iter().sum::<u8>() as usize
+    }
+}
+
 fn sum_group_counts(groups: &[Vec<Vec<char>>]) -> usize {
     groups.iter().map(|group| count_unique(group)).sum()
 }
 
 fn count_unique(group: &[Vec<char>]) -> usize {
-    let mut unique_elements = HashSet::new();
+    let mut unique_elements = AlphabetSet::new();
 
     for person in group {
-        for answer in person {
+        for &answer in person {
             unique_elements.insert(answer);
         }
     }

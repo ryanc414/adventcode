1c3c39747afbf6cf6a00b5e91c2f418addb86eb5
Ryan Collingham <ryan@tokencard.io>
2020-12-13 11:54:15 +0000

    solve part 2 of p12

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -3,8 +3,12 @@ use std::fs;
 
 fn main() {
     let input = load_input();
-    let final_distance = find_final_manhattan_distance(&input);
-    println!("final distance is {}", final_distance);
+
+    let final_distance_1 = find_final_manhattan_distance(&input, State1::new());
+    println!("final distance is {}", final_distance_1);
+
+    let final_distance_2 = find_final_manhattan_distance(&input, State2::new());
+    println!("final distance is {}", final_distance_2);
 }
 
 struct Instruction {
@@ -61,22 +65,42 @@ fn load_input() -> Vec<Instruction> {
         .collect()
 }
 
+trait State {
+    fn process_instruction(&self, instr: &Instruction) -> Self;
+    fn manhattan_distance(&self) -> i64;
+}
+
 #[derive(Debug)]
-struct State {
+struct State1 {
     position: (i64, i64),
     facing: usize,
 }
 
 const DIRECTIONS: [(i64, i64); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-impl State {
+impl State1 {
     fn new() -> Self {
-        State {
+        Self {
             position: (0, 0),
             facing: 0,
         }
     }
 
+    fn turn(&self, degrees: i64, right: bool) -> usize {
+        if degrees % 90 != 0 {
+            panic!("unexpected turn value {}", degrees);
+        }
+
+        if !right {
+            ((self.facing as i64 + (degrees / 90)) % 4) as usize
+        } else {
+            let a = self.facing as i64 - ((degrees % 360) / 90);
+            ((a + 4) % 4) as usize
+        }
+    }
+}
+
+impl State for State1 {
     fn process_instruction(&self, instr: &Instruction) -> Self {
         match instr.action {
             Action::North => Self {
@@ -113,16 +137,98 @@ impl State {
         }
     }
 
-    fn turn(&self, degrees: i64, right: bool) -> usize {
-        if degrees % 90 != 0 {
-            panic!("unexpected turn value {}", degrees);
-        }
+    fn manhattan_distance(&self) -> i64 {
+        self.position.0.abs() + self.position.1.abs()
+    }
+}
 
-        if !right {
-            ((self.facing as i64 + (degrees / 90)) % 4) as usize
+struct State2 {
+    position: (i64, i64),
+    waypoint: (i64, i64),
+}
+
+impl State2 {
+    fn new() -> Self {
+        Self {
+            position: (0, 0),
+            waypoint: (10, 1),
+        }
+    }
+
+    fn turn(&self, degrees: i64, right: bool) -> (i64, i64) {
+        if degrees % 90 != 0 {
+            panic!("unexpected degrees value");
+        }
+        let turns = (degrees % 360) / 90;
+        let mat = if !right {
+            // println!(
+            //     "turning left by {} degrees -> rotation matrix {}",
+            //     degrees, turns
+            // );
+            ROTATION_MATRICES[turns as usize]
         } else {
-            let a = self.facing as i64 - ((degrees % 360) / 90);
-            ((a + 4) % 4) as usize
+            let i = ((4 - turns) % 4) as usize;
+            // println!(
+            //     "turning right by {} degrees -> rotation matrix {}",
+            //     degrees, i
+            // );
+            ROTATION_MATRICES[i]
+        };
+
+        (
+            (self.waypoint.0 * mat[0][0]) + (self.waypoint.1 * mat[0][1]),
+            (self.waypoint.0 * mat[1][0]) + (self.waypoint.1 * mat[1][1]),
+        )
+    }
+}
+
+const ROTATION_MATRICES: [[[i64; 2]; 2]; 4] = [
+    [[1, 0], [0, 1]],   // 0 degrees
+    [[0, -1], [1, 0]],  // 90 degrees
+    [[-1, 0], [0, -1]], // 180 degrees
+    [[0, 1], [-1, 0]],  // 270 degrees
+];
+
+impl State for State2 {
+    fn process_instruction(&self, instr: &Instruction) -> Self {
+        match instr.action {
+            Action::North => Self {
+                position: self.position,
+                waypoint: (self.waypoint.0, self.waypoint.1 + instr.value),
+            },
+
+            Action::South => Self {
+                position: self.position,
+                waypoint: (self.waypoint.0, self.waypoint.1 - instr.value),
+            },
+
+            Action::East => Self {
+                position: self.position,
+                waypoint: (self.waypoint.0 + instr.value, self.waypoint.1),
+            },
+
+            Action::West => Self {
+                position: self.position,
+                waypoint: (self.waypoint.0 - instr.value, self.waypoint.1),
+            },
+
+            Action::Left => Self {
+                position: self.position,
+                waypoint: self.turn(instr.value, false),
+            },
+
+            Action::Right => Self {
+                position: self.position,
+                waypoint: self.turn(instr.value, true),
+            },
+
+            Action::Forward => Self {
+                position: (
+                    self.position.0 + (self.waypoint.0 * instr.value),
+                    self.position.1 + (self.waypoint.1 * instr.value),
+                ),
+                waypoint: self.waypoint,
+            },
         }
     }
 
@@ -131,9 +237,7 @@ impl State {
     }
 }
 
-fn find_final_manhattan_distance(input: &[Instruction]) -> i64 {
-    let mut state = State::new();
-
+fn find_final_manhattan_distance<S: State>(input: &[Instruction], mut state: S) -> i64 {
     for instr in input {
         state = state.process_instruction(instr);
     }

4572775cfd2b3157bf4e429f23b01892a7121221
Ryan Collingham <ryan@tokencard.io>
2020-12-13 20:08:25 +0000

    solve part 2 of p13

--- p13/src/main.rs
+++ p13/src/main.rs
@@ -3,28 +3,42 @@ use std::fs;
 
 fn main() {
     let input = load_input();
+
     let result = find_earliest_bus(&input);
     println!("result is {}", result);
+
+    let timestamp = find_earliest_timestamp(&input);
+    println!("earliest timestamp that matches rules is {}", timestamp);
 }
 
 struct BusTimetable {
     earliest_timestamp: u64,
-    bus_ids: Vec<u64>,
+    buses: Vec<BusInfo>,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct BusInfo {
+    id: u64,
+    list_position: u64,
 }
 
 impl BusTimetable {
     fn parse(input: &str) -> Self {
         let lines: Vec<&str> = input.split('\n').collect();
         let earliest_timestamp: u64 = lines[0].parse().unwrap();
-        let bus_ids = lines[1]
+        let buses = lines[1]
             .split(',')
-            .filter(|&id| id != "x")
-            .map(|id| id.parse().unwrap())
+            .enumerate()
+            .filter(|&(_, id)| id != "x")
+            .map(|(i, id)| BusInfo {
+                id: id.parse().unwrap(),
+                list_position: i as u64,
+            })
             .collect();
 
         Self {
             earliest_timestamp,
-            bus_ids,
+            buses,
         }
     }
 }
@@ -37,14 +51,14 @@ fn load_input() -> BusTimetable {
 
 fn find_earliest_bus(input: &BusTimetable) -> u64 {
     let (min_wait, id) = input
-        .bus_ids
+        .buses
         .iter()
         .fold(
             None,
-            |current_min: Option<(u64, u64)>, id: &u64| -> Option<(u64, u64)> {
-                let wait = id - (input.earliest_timestamp % id);
+            |current_min: Option<(u64, u64)>, bus: &BusInfo| -> Option<(u64, u64)> {
+                let wait = bus.id - (input.earliest_timestamp % bus.id);
                 if current_min.is_none() || wait < current_min.unwrap().0 {
-                    Some((wait, *id))
+                    Some((wait, bus.id))
                 } else {
                     current_min
                 }
@@ -53,4 +67,45 @@ fn find_earliest_bus(input: &BusTimetable) -> u64 {
         .unwrap();
 
     min_wait * id
+}
+
+fn find_earliest_timestamp(input: &BusTimetable) -> u64 {
+    let mut bus_info = input.buses.to_vec();
+    bus_info.sort_unstable_by(|bus_a, bus_b| bus_b.id.cmp(&bus_a.id));
+
+    find_earliest_timestamp_from(
+        &bus_info[1..],
+        bus_info[0].id - bus_info[0].list_position,
+        bus_info[0].id,
+    )
+}
+
+fn find_earliest_timestamp_from(buses: &[BusInfo], start_timestamp: u64, increment: u64) -> u64 {
+    if buses.is_empty() {
+        return start_timestamp;
+    }
+
+    let head_bus = buses[0];
+    let tail_buses = &buses[1..];
+    let mut t = start_timestamp;
+
+    loop {
+        if (t + head_bus.list_position) % head_bus.id == 0 {
+            let next_increment = lowest_common_multiple(increment, head_bus.id);
+            return find_earliest_timestamp_from(tail_buses, t, next_increment);
+        }
+
+        t += increment;
+    }
+}
+
+fn lowest_common_multiple(x: u64, y: u64) -> u64 {
+    let mut m = x;
+
+    loop {
+        if m % y == 0 {
+            return m;
+        }
+        m += x;
+    }
 }
